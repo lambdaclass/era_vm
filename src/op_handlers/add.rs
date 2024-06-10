e8ddeab9ff4e30d3bc12c47682b3079a9d0e0089
Gianbelinche <gianluca.belinche@lambdaclass.com>
2024-06-10 16:29:23 -0300

    Add initial add opcode

--- src/op_handlers/add.rs
+++ src/op_handlers/add.rs
@@ -1,28 +1,78 @@
 use crate::{opcode::Opcode, state::VMState};
 use zkevm_opcode_defs::Operand;
 use zkevm_opcode_defs::ImmMemHandlerFlags;
+use zkevm_opcode_defs::RegOrImmFlags;
+use u256::U256;
+
+
+
+fn _add_reg_only(vm: &mut VMState, opcode: Opcode) {
+    // src0 + src1 -> dst0
+    let src0 = vm.get_register(opcode.src0_index);
+    let src1 = vm.get_register(opcode.src1_index);
+    vm.set_register(opcode.dst0_index, src0 + src1);
+}
+
+fn _add_imm16_only(vm: &mut VMState, opcode: Opcode) {
+    // imm0 + src0 -> dst0
+    let src1 = vm.get_register(opcode.src1_index);
+    vm.set_register(opcode.dst0_index, src1 + opcode.imm0);
+}
 
 pub fn _add(vm: &mut VMState, opcode: Opcode) {
     match opcode.src0_operand_type {
-        Operand::RegOnly => todo!(),
-        Operand::RegOrImm(_) => todo!(),
+        Operand::RegOnly => {
+            _add_reg_only(vm, opcode);
+        },
+        Operand::RegOrImm(variant) => {
+            match variant {
+                RegOrImmFlags::UseRegOnly => {
+                    _add_reg_only(vm, opcode);
+                },
+                RegOrImmFlags::UseImm16Only => {
+                    _add_imm16_only(vm, opcode);
+                },
+            }
+        },
         Operand::Full(variant) => {
             match variant {
                 ImmMemHandlerFlags::UseRegOnly => {
-                    // src0 + src1 -> dst0
+                    _add_reg_only(vm, opcode);
+                },
+                ImmMemHandlerFlags::UseStackWithPushPop => {
+                    // stack-=[src0 + offset] + src1 -> dst0
+                    panic!("Add with UseStackWithPushPop is not allowed")
+                },
+                ImmMemHandlerFlags::UseStackWithOffset => {
+                    // stack[src0 + offset] + src1 -> dst0
                     let src0 = vm.get_register(opcode.src0_index);
                     let src1 = vm.get_register(opcode.src1_index);
-                    vm.set_register(opcode.dst0_index, src0 + src1);
+                    let offset = opcode.imm0; 
+
+                    let sp = vm.current_frame.stack.len();
+                    let res = vm.current_frame.stack[sp - (src0 + U256::from(offset)).as_usize()].value.clone();
+                    vm.set_register(opcode.dst0_index, res + src1);
                 },
-                ImmMemHandlerFlags::UseStackWithPushPop => todo!(),
-                ImmMemHandlerFlags::UseStackWithOffset => todo!(),
-                ImmMemHandlerFlags::UseAbsoluteOnStack => todo!(),
-                ImmMemHandlerFlags::UseImm16Only => {
-                    // imm0 + src0 -> dst0
+                ImmMemHandlerFlags::UseAbsoluteOnStack => {
+                    // stack=[src0 + offset] + src1 -> dst0
                     let src0 = vm.get_register(opcode.src0_index);
-                    vm.set_register(opcode.dst0_index, src0 + opcode.imm0);
+                    let src1 = vm.get_register(opcode.src1_index);
+                    let offset = opcode.imm0; 
+
+                    let res = vm.current_frame.stack[(src0 + U256::from(offset)).as_usize()].value.clone();
+                    vm.set_register(opcode.dst0_index, res + src1);
                 },
-                ImmMemHandlerFlags::UseCodePage => todo!(),
+                ImmMemHandlerFlags::UseImm16Only => {
+                    _add_imm16_only(vm, opcode);
+                },
+                ImmMemHandlerFlags::UseCodePage => {
+                    let src0 = vm.get_register(opcode.src0_index);
+                    let src1 = vm.get_register(opcode.src1_index);
+                    let offset = opcode.imm0; 
+
+                    let res = vm.current_frame.code_page[(src0 + U256::from(offset)).as_usize()]; // to u256?
+                    vm.set_register(opcode.dst0_index, res + src1);
+                },
             }
         },
     }
