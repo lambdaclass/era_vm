3200c716ad321601c4d137c1f2461df298f59a34
Francisco Krause Arnim <56402156+fkrause98@users.noreply.github.com>
2024-06-12 15:34:21 -0300

    Add target for asm (#17)
    * Add sub program
    
    * Add 'add' program
    
    * Update makefile with asm target
    
    * Update test setup, add failing sub test and 'add' test
    
    * Cargo fmt
    
    * Remove echo
    
    * Clippy

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,9 +1,40 @@
 use era_vm::run_program;
 use u256::U256;
+const ARTIFACTS_PATH: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/program_artifacts");
+
+fn make_bin_path_yul(file_name: &str) -> String {
+    format!(
+        "{}/{}.artifacts.yul/{}.yul.zbin",
+        ARTIFACTS_PATH, file_name, file_name
+    )
+}
+
+fn make_bin_path_asm(file_name: &str) -> String {
+    format!(
+        "{}/{}.artifacts.zasm/{}.zasm.zbin",
+        ARTIFACTS_PATH, file_name, file_name
+    )
+}
 
 #[test]
-fn test_add() {
-    let bin_path = "./program_artifacts/add.artifacts/add.yul.zbin";
-    let result = run_program(bin_path);
+fn test_add_yul() {
+    let bin_path = make_bin_path_yul("add");
+    dbg!(&bin_path);
+    let result = run_program(&bin_path);
+    assert_eq!(result, U256::from_dec_str("3").unwrap());
+}
+
+#[test]
+fn test_add_asm() {
+    let bin_path = make_bin_path_asm("add");
+    let result = run_program(&bin_path);
+    assert_eq!(result, U256::from_dec_str("3").unwrap());
+}
+
+#[test]
+#[should_panic]
+fn test_sub_asm() {
+    let bin_path = make_bin_path_asm("sub");
+    let result = run_program(&bin_path);
     assert_eq!(result, U256::from_dec_str("3").unwrap());
 }
