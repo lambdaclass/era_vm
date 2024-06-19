322f60518d78a99b142ec47546ba83feb57b07d9
Francisco Krause Arnim <56402156+fkrause98@users.noreply.github.com>
2024-06-19 13:17:44 -0300

    Add simple gas tracking (#28)
    * Add simple gas tracking
    
    * Add simple gas tracking and some tests for it
    
    * Cargo fmt
    
    * Clippy
    
    * Cargo fmt
    
    * Cargo clippy
    
    * Remove dbg
    
    * Add comment for test

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -263,3 +263,21 @@ fn test_add_does_not_modify_set_flags() {
     assert!(final_vm_state.flag_lt_of);
     assert!(final_vm_state.flag_eq);
 }
+#[test]
+// This test should run out of gas before
+// the program can save a number 3 into the storage.
+fn test_runs_out_of_gas_and_stops() {
+    let bin_path = make_bin_path_asm("add_with_costs");
+    let vm = VMState::new_with_gas(5510);
+    let (result, _) = run_program_with_custom_state(&bin_path, vm);
+    assert_eq!(result, U256::from_dec_str("0").unwrap());
+}
+
+#[test]
+fn test_uses_expected_gas() {
+    let bin_path = make_bin_path_asm("add_with_costs");
+    let vm = VMState::new_with_gas(5600);
+    let (result, final_vm_state) = run_program_with_custom_state(&bin_path, vm);
+    assert_eq!(result, U256::from_dec_str("3").unwrap());
+    assert_eq!(final_vm_state.gas_left(), 0_u32);
+}
