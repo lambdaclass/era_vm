2bc8b10238d8ccedbb5a647e438cddc68a403a79
IAvecilla <ignacio.avecilla@lambdaclass.com>
2024-06-25 11:58:58 -0300

    Get values for storage actions

--- src/op_handlers/log.rs
+++ src/op_handlers/log.rs
@@ -3,12 +3,12 @@ use u256::U256;
 use crate::{state::VMState, store::Storage, value::TaggedValue, Opcode};
 
 pub fn _storage_write(vm: &mut VMState, opcode: &Opcode) {
-    let key = vm.get_register(opcode.src0_index);
-    let value = vm.get_register(opcode.src1_index);
+    let key = vm.get_register(opcode.src0_index).value;
+    let value = vm.get_register(opcode.src1_index).value;
     vm.current_frame
         .storage
         .borrow_mut()
-        .store(key.value, value.value)
+        .store(key, value)
         .unwrap();
 }
 
@@ -24,20 +24,20 @@ pub fn _storage_read(vm: &mut VMState, opcode: &Opcode) {
 }
 
 pub fn _transient_storage_write(vm: &mut VMState, opcode: &Opcode) {
-    let key = vm.get_register(opcode.src0_index);
-    let value = vm.get_register(opcode.src1_index);
+    let key = vm.get_register(opcode.src0_index).value;
+    let value = vm.get_register(opcode.src1_index).value;
     vm.current_frame
         .transient_storage
-        .store(key.value, value.value)
+        .store(key, value)
         .unwrap();
 }
 
 pub fn _transient_storage_read(vm: &mut VMState, opcode: &Opcode) {
-    let key = vm.get_register(opcode.src0_index);
+    let key = vm.get_register(opcode.src0_index).value;
     let value = vm
         .current_frame
         .transient_storage
-        .read(&key.value)
+        .read(&key)
         .unwrap_or(U256::zero());
     vm.set_register(opcode.dst0_index, TaggedValue::new_raw_integer(value));
 }
