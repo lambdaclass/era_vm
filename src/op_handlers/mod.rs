7b9e4b6459e8bb136b4cf793c805a49d8d7e7009
juan518munoz <62400508+juan518munoz@users.noreply.github.com>
2024-06-26 12:29:18 -0300

    binop & jump implementation (#32)
    * initial commit
    
    * inital jump impl
    
    * jump label test
    
    * binop set flags
    
    * fmt
    
    * add EOF
    
    * remove prints
    
    * change import
    
    * fix integration tests
    
    * refine jump test
    
    * fix main merge

--- src/op_handlers/mod.rs
+++ src/op_handlers/mod.rs
@@ -1,9 +1,13 @@
 pub mod add;
+pub mod and;
 pub mod div;
+pub mod jump;
 pub mod log;
 pub mod mul;
+pub mod or;
 pub mod ptr_add;
 pub mod ptr_pack;
 pub mod ptr_shrink;
 pub mod ptr_sub;
 pub mod sub;
+pub mod xor;
