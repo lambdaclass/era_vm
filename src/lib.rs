850ea74cc80c894895f3afa6ecb07af81443b420
Jordi <jordi.bonet.valiente@lambdaclass.com>
2024-06-26 16:41:59 +0200

    Relocate shift match

--- src/lib.rs
+++ src/lib.rs
@@ -12,7 +12,10 @@ use op_handlers::ptr_add::_ptr_add;
 use op_handlers::ptr_pack::_ptr_pack;
 use op_handlers::ptr_shrink::_ptr_shrink;
 use op_handlers::ptr_sub::_ptr_sub;
-use op_handlers::shift::_shift;
+use op_handlers::shift::_rol;
+use op_handlers::shift::_ror;
+use op_handlers::shift::_shl;
+use op_handlers::shift::_shr;
 use op_handlers::sub::_sub;
 pub use opcode::Opcode;
 use state::VMState;
@@ -22,6 +25,7 @@ use zkevm_opcode_defs::ISAVersion;
 use zkevm_opcode_defs::LogOpcode;
 use zkevm_opcode_defs::Opcode as Variant;
 use zkevm_opcode_defs::PtrOpcode;
+use zkevm_opcode_defs::ShiftOpcode;
 
 /// Run a vm program with a clean VM state.
 pub fn run_program(bin_path: &str) -> (U256, VMState) {
@@ -68,14 +72,13 @@ pub fn run_program_with_custom_state(bin_path: &str, mut vm: VMState) -> (U256, 
                 Variant::Div(_) => _div(&mut vm, &opcode),
                 Variant::Jump(_) => todo!(),
                 Variant::Context(_) => todo!(),
-                Variant::Shift(_) => _shift(
-                    &mut vm,
-                    &opcode,
-                    match opcode.variant {
-                        Variant::Shift(shift_type) => shift_type,
-                        _ => unreachable!(),
-                    },
-                ),
+                Variant::Shift(_) => match opcode.variant {
+                    Variant::Shift(ShiftOpcode::Shl) => _shl(&mut vm, &opcode),
+                    Variant::Shift(ShiftOpcode::Shr) => _shr(&mut vm, &opcode),
+                    Variant::Shift(ShiftOpcode::Rol) => _rol(&mut vm, &opcode),
+                    Variant::Shift(ShiftOpcode::Ror) => _ror(&mut vm, &opcode),
+                    _ => unreachable!(),
+                },
                 Variant::Binop(_) => todo!(),
                 Variant::Ptr(ptr_variant) => match ptr_variant {
                     PtrOpcode::Add => _ptr_add(&mut vm, &opcode),
--- src/op_handlers/shift.rs
+++ src/op_handlers/shift.rs
@@ -1,9 +1,8 @@
 use crate::address_operands::{address_operands_read, address_operands_store};
 use crate::value::TaggedValue;
 use crate::{opcode::Opcode, state::VMState};
-use zkevm_opcode_defs::ShiftOpcode;
 
-fn _shl(vm: &mut VMState, opcode: &Opcode) {
+pub fn _shl(vm: &mut VMState, opcode: &Opcode) {
     let (src0_t, src1_t) = address_operands_read(vm, opcode);
     let (src0, src1) = (src0_t.value, src1_t.value);
     let shift = src1.low_u32() % 256;
@@ -18,7 +17,7 @@ fn _shl(vm: &mut VMState, opcode: &Opcode) {
     address_operands_store(vm, opcode, TaggedValue::new_raw_integer(res));
 }
 
-fn _shr(vm: &mut VMState, opcode: &Opcode) {
+pub fn _shr(vm: &mut VMState, opcode: &Opcode) {
     let (src0_t, src1_t) = address_operands_read(vm, opcode);
     let (src0, src1) = (src0_t.value, src1_t.value);
     let shift = src1.low_u32() % 256;
@@ -33,7 +32,7 @@ fn _shr(vm: &mut VMState, opcode: &Opcode) {
     address_operands_store(vm, opcode, TaggedValue::new_raw_integer(res));
 }
 
-fn _rol(vm: &mut VMState, opcode: &Opcode) {
+pub fn _rol(vm: &mut VMState, opcode: &Opcode) {
     let (src0_t, src1_t) = address_operands_read(vm, opcode);
     let (src0, src1) = (src0_t.value, src1_t.value);
     let shift = src1.low_u32() % 256;
@@ -48,7 +47,7 @@ fn _rol(vm: &mut VMState, opcode: &Opcode) {
     address_operands_store(vm, opcode, TaggedValue::new_raw_integer(result));
 }
 
-fn _ror(vm: &mut VMState, opcode: &Opcode) {
+pub fn _ror(vm: &mut VMState, opcode: &Opcode) {
     let (src0_t, src1_t) = address_operands_read(vm, opcode);
     let (src0, src1) = (src0_t.value, src1_t.value);
     let shift = src1.low_u32() % 256;
@@ -61,13 +60,4 @@ fn _ror(vm: &mut VMState, opcode: &Opcode) {
         vm.flag_gt = false;
     }
     address_operands_store(vm, opcode, TaggedValue::new_raw_integer(result));
-}
-
-pub fn _shift(vm: &mut VMState, opcode: &Opcode, shift_type: ShiftOpcode) {
-    match shift_type {
-        ShiftOpcode::Shl => _shl(vm, opcode),
-        ShiftOpcode::Shr => _shr(vm, opcode),
-        ShiftOpcode::Rol => _rol(vm, opcode),
-        ShiftOpcode::Ror => _ror(vm, opcode),
-    }
 }
