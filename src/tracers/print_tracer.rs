9a3813516994e593144c9e3df8b0ad4077922616
Gianbelinche <39842759+gianbelinche@users.noreply.github.com>
2024-06-28 10:49:30 -0300

    Merge near call

--- src/tracers/print_tracer.rs
+++ src/tracers/print_tracer.rs
@@ -29,8 +29,8 @@ impl Tracer for PrintTracer {
                 let fat_ptr = FatPointer::decode(src0.value);
                 if fat_ptr.offset == DEBUG_SLOT {
                     let how_to_print_value =
-                        new_vm.current_context_mut().heap.read(DEBUG_SLOT + 32);
-                    let value_to_print = new_vm.current_context_mut().heap.read(DEBUG_SLOT + 64);
+                        new_vm.current_frame_mut().heap.read(DEBUG_SLOT + 32);
+                    let value_to_print = new_vm.current_frame_mut().heap.read(DEBUG_SLOT + 64);
 
                     let print_as_hex_value = U256::from_str_radix(
                         "0x00debdebdebdebdebdebdebdebdebdebdebdebdebdebdebdebdebdebdebdebde",
