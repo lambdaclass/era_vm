8738325ff3ef6d34db19badd35d92469bd12731b
Gianbelinche <39842759+gianbelinche@users.noreply.github.com>
2024-07-17 15:13:57 -0300

    Fixed div flags

--- src/op_handlers/div.rs
+++ src/op_handlers/div.rs
@@ -1,3 +1,6 @@
+
+use u256::U256;
+
 use crate::address_operands::{address_operands_div_mul, address_operands_read};
 use crate::value::TaggedValue;
 use crate::{opcode::Opcode, state::VMState};
@@ -9,10 +12,10 @@ pub fn div(vm: &mut VMState, opcode: &Opcode) {
     if opcode.alters_vm_flags {
         // Lt overflow is cleared
         vm.flag_lt_of = false;
-        // Eq is set if quotient is not zero
-        vm.flag_eq = !quotient.is_zero();
-        // Gt is set if the remainder is not zero
-        vm.flag_gt = !remainder.is_zero();
+        // Eq is set if quotient is zero
+        vm.flag_eq = quotient.is_zero();
+        // Gt is set if the remainder is zero
+        vm.flag_gt = remainder.is_zero();
     }
 
     address_operands_div_mul(
