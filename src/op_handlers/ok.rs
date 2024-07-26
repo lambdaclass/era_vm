53707d7825217492ae0f10a79ab4206987130e19
Gianbelinche <39842759+gianbelinche@users.noreply.github.com>
2024-07-26 12:33:23 -0300

    Refactor in near and far call

--- src/op_handlers/ok.rs
+++ src/op_handlers/ok.rs
@@ -6,24 +6,21 @@ pub fn ok(vm: &mut VMState, opcode: &Opcode) -> Result<bool, EraVmError> {
     vm.flag_eq = false;
     vm.flag_lt_of = false;
     vm.flag_gt = false;
-    if vm.running_contexts.len() > 1 || !vm.current_context()?.near_call_frames.is_empty() {
-        if !vm.current_context()?.near_call_frames.is_empty() {
-            // Near call
-            let previous_frame = vm.pop_frame()?;
-            if opcode.alters_vm_flags {
-                // Marks if it has .to_label
-                let to_label = opcode.imm0;
-                vm.current_frame_mut()?.pc = (to_label - 1) as u64; // To account for the +1 later
-            } else {
-                vm.current_frame_mut()?.pc -= 1; // To account for the +1 later
-            }
-            vm.current_frame_mut()?.gas_left += previous_frame.gas_left;
+    if vm.in_near_call()? {
+        let previous_frame = vm.pop_frame()?;
+        if opcode.alters_vm_flags {
+            // Marks if it has .to_label
+            let to_label = opcode.imm0;
+            vm.current_frame_mut()?.pc = (to_label - 1) as u64; // To account for the +1 later
         } else {
-            // Far call
-            perform_return(vm, opcode)?;
-            vm.register_context_u128 = 0_u128;
-            vm.pop_frame()?;
+            vm.current_frame_mut()?.pc -= 1; // To account for the +1 later
         }
+        vm.current_frame_mut()?.gas_left += previous_frame.gas_left;
+        Ok(false)
+    } else if vm.in_far_call() {
+        perform_return(vm, opcode)?;
+        vm.register_context_u128 = 0_u128;
+        vm.pop_frame()?;
         Ok(false)
     } else {
         perform_return(vm, opcode)?;
--- src/op_handlers/revert.rs
+++ src/op_handlers/revert.rs
@@ -13,29 +13,27 @@ pub fn revert(vm: &mut VMState, opcode: &Opcode) -> Result<bool, EraVmError> {
     vm.flag_eq = false;
     vm.flag_lt_of = false;
     vm.flag_gt = false;
-    if vm.running_contexts.len() > 1 || !vm.current_context()?.near_call_frames.is_empty() {
-        if !vm.current_context()?.near_call_frames.is_empty() {
-            // Near call
-            let previous_frame = vm.pop_frame()?;
-            if opcode.alters_vm_flags {
-                // Marks if it has .to_label
-                let to_label = opcode.imm0;
-                vm.current_frame_mut()?.pc = (to_label - 1) as u64;
-            // To account for the +1 later
-            } else {
-                vm.current_frame_mut()?.pc = previous_frame.exception_handler - 1;
-                // To account for the +1 later
-            }
-            vm.current_frame_mut()?.gas_left += previous_frame.gas_left;
+    if vm.in_near_call()? {
+        let previous_frame = vm.pop_frame()?;
+        if opcode.alters_vm_flags {
+            // Marks if it has .to_label
+            let to_label = opcode.imm0;
+            vm.current_frame_mut()?.pc = (to_label - 1) as u64;
+        // To account for the +1 later
         } else {
-            let register = vm.get_register(opcode.src0_index);
-            let result = get_forward_memory_pointer(register.value, vm, register.is_pointer)?;
-            vm.clear_registers();
-            vm.set_register(1, TaggedValue::new_pointer(FatPointer::encode(&result)));
-            vm.flag_lt_of = true;
-            let previous_frame = vm.pop_frame()?;
-            vm.current_frame_mut()?.pc = previous_frame.exception_handler;
+            vm.current_frame_mut()?.pc = previous_frame.exception_handler - 1;
+            // To account for the +1 later
         }
+        vm.current_frame_mut()?.gas_left += previous_frame.gas_left;
+        Ok(false)
+    } else if vm.in_far_call() {
+        let register = vm.get_register(opcode.src0_index);
+        let result = get_forward_memory_pointer(register.value, vm, register.is_pointer)?;
+        vm.clear_registers();
+        vm.set_register(1, TaggedValue::new_pointer(FatPointer::encode(&result)));
+        vm.flag_lt_of = true;
+        let previous_frame = vm.pop_frame()?;
+        vm.current_frame_mut()?.pc = previous_frame.exception_handler;
         Ok(false)
     } else {
         perform_return(vm, opcode)?;
