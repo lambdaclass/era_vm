aec6be1e0d32762865045f3fe59e94fdbd4dc5d0
Jordi <jordi.bonet.valiente@lambdaclass.com>
2024-07-30 17:37:40 +0200

    Use a better approach

--- src/op_handlers/far_call.rs
+++ src/op_handlers/far_call.rs
@@ -66,31 +66,29 @@ pub fn get_forward_memory_pointer(
                 return Err(HeapError::StoreOutOfBounds.into());
             };
 
-            if pointer.offset != 0 {
-                return Err(HeapError::StoreOutOfBounds.into());
-            }
+            if !is_pointer && pointer.offset == 0 {
+                let ergs_cost = match pointer_kind {
+                    PointerSource::NewForHeap => {
+                        pointer.page = vm.current_frame()?.heap_id;
+                        vm.heaps
+                            .get_mut(vm.current_frame()?.heap_id)
+                            .ok_or(HeapError::StoreOutOfBounds)?
+                            .expand_memory(bound)
+                    }
+                    PointerSource::NewForAuxHeap => {
+                        pointer.page = vm.current_frame()?.aux_heap_id;
+                        vm.heaps
+                            .get_mut(vm.current_frame()?.aux_heap_id)
+                            .ok_or(HeapError::StoreOutOfBounds)?
+                            .expand_memory(pointer.start + pointer.len)
+                    }
+                    _ => unreachable!(),
+                };
 
-            let ergs_cost = match pointer_kind {
-                PointerSource::NewForHeap => {
-                    pointer.page = vm.current_frame()?.heap_id;
-                    vm.heaps
-                        .get_mut(vm.current_frame()?.heap_id)
-                        .ok_or(HeapError::StoreOutOfBounds)?
-                        .expand_memory(bound)
+                let underflows = vm.decrease_gas(ergs_cost)?;
+                if underflows {
+                    return Err(HeapError::StoreOutOfBounds.into());
                 }
-                PointerSource::NewForAuxHeap => {
-                    pointer.page = vm.current_frame()?.aux_heap_id;
-                    vm.heaps
-                        .get_mut(vm.current_frame()?.aux_heap_id)
-                        .ok_or(HeapError::StoreOutOfBounds)?
-                        .expand_memory(pointer.start + pointer.len)
-                }
-                _ => unreachable!(),
-            };
-
-            let underflows = vm.decrease_gas(ergs_cost)?;
-            if underflows {
-                return Err(HeapError::StoreOutOfBounds.into());
             }
         }
     };
