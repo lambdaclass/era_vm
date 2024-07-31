1d6599399d8a8397d5707182a5ead478b4123bcb
Julian Ventura <43799596+JulianVentura@users.noreply.github.com>
2024-07-31 10:56:12 -0300

    Add `OpDelegateCall` implementation. (#130)
    Co-authored-by: Julian Ventura <julian.ventura@lambdaclass.com>

--- src/call_frame.rs
+++ src/call_frame.rs
@@ -45,6 +45,7 @@ impl Context {
         program_code: Vec<U256>,
         gas_stipend: u32,
         contract_address: Address,
+        code_address: Address,
         caller: Address,
         heap_id: u32,
         aux_heap_id: u32,
@@ -65,7 +66,7 @@ impl Context {
             near_call_frames: vec![],
             contract_address,
             caller,
-            code_address: contract_address,
+            code_address,
             context_u128,
             stack: Stack::new(),
         }
