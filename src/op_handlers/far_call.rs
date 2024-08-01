820ccd157fac92eaf20c74cfc752748c879592a0
IAvecilla <ignacio.avecilla@lambdaclass.com>
2024-08-01 11:17:40 -0300

    Update decommit process to check for zero code bytes and use aa default code hash

--- src/op_handlers/far_call.rs
+++ src/op_handlers/far_call.rs
@@ -146,7 +146,8 @@ pub fn far_call(
 
     let exception_handler = opcode.imm0 as u64;
 
-    let abi = get_far_call_arguments(src0.value);
+    let mut abi = get_far_call_arguments(src0.value);
+    abi.is_constructor_call = abi.is_constructor_call == vm.current_frame()?.is_kernel;
 
     let deployer_system_contract_address =
         Address::from_low_u64_be(DEPLOYER_SYSTEM_CONTRACT_ADDRESS_LOW as u64);
@@ -160,6 +161,41 @@ pub fn far_call(
         .ok_or(StorageError::KeyNotPresent)?;
     let mut code_info_bytes = [0; 32];
     code_info.to_big_endian(&mut code_info_bytes);
+
+    // Note that EOAs are considered constructed because their code info is all zeroes.
+    let is_constructed = match code_info_bytes[1] {
+        0 => true,
+        1 => false,
+        _ => {
+            return Err(EraVmError::IncorrectBytecodeFormat);
+        }
+    };
+    let default_aa_code_hash: [u8; 32] = [1, 0, 5, 99, 238, 93, 10, 189, 249, 252, 101, 247, 72, 167, 0, 210, 195, 119, 170, 222, 188, 22, 213, 191, 33, 16, 95, 138, 148, 239, 240, 40];
+    let try_default_aa = if contract_address.0[..18].iter().all(|&byte| byte == 0) {
+        None
+    } else {
+        Some(default_aa_code_hash)
+    };
+
+    // The address aliasing contract implements Ethereum-like behavior of calls to EOAs
+    // returning successfully (and address aliasing when called from the bootloader).
+    // It makes sense that unconstructed code is treated as an EOA but for some reason
+    // a constructor call to constructed code is also treated as EOA.
+    code_info_bytes = match code_info_bytes[0] {
+        1 => {
+            if is_constructed == abi.is_constructor_call {
+                try_default_aa.ok_or(StorageError::KeyNotPresent)?
+            } else {
+                code_info_bytes
+            }
+        }
+        2 => {
+            // This will change after 1.5 and evm_interpreter_code_hash should be used. Now there are the same.
+            try_default_aa.ok_or(StorageError::KeyNotPresent)?
+        }
+        _ if code_info == U256::zero() => try_default_aa.ok_or(StorageError::KeyNotPresent)?,
+        _ => return Err(EraVmError::IncorrectBytecodeFormat),
+    };
 
     code_info_bytes[1] = 0;
     let code_key: U256 = U256::from_big_endian(&code_info_bytes);
