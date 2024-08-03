24c88e52403a55096d3956eeecb62705b15e9cba
Marcos Nicolau <76252340+MarcosNicolau@users.noreply.github.com>
2024-08-02 22:03:36 -0300

    Optimize PC assignment by using low_u64()
    Co-authored-by: Mario Rugiero <mrugiero@gmail.com>

--- src/op_handlers/jump.rs
+++ src/op_handlers/jump.rs
@@ -8,7 +8,7 @@ use crate::{opcode::Opcode, state::VMState};
 pub fn jump(vm: &mut VMState, opcode: &Opcode) -> Result<(), EraVmError> {
     let (src0, _) = address_operands_read(vm, opcode)?;
 
-    let next_pc = src0.value & U256::from(u64::MAX);
-    vm.current_frame_mut()?.pc = next_pc.as_u64();
+    let next_pc = src0.value.low_u64();
+    vm.current_frame_mut()?.pc = next_pc;
     address_operands_store(vm, opcode, TaggedValue::new_raw_integer(next_pc))
 }
