0788fbd0aa13e4c0cd75d7679d4d6e37ef0252bc
Marcos Nicolau <marcosnicolau@lambdaclass.com>
2024-08-04 22:28:29 -0300

    Fix ret opcode
    This wasn't breaking any test, but when doing reverts and ok, we were not clearing the registers and the context as specified in the spec. Since ret.panic, ret.revert and ret.ok have very similar behavior, I though it was proper to unify them in ret to make sure we ran the same common behaviour in all of them.

--- src/lib.rs
+++ src/lib.rs
@@ -35,15 +35,13 @@ use op_handlers::log::{
 
 use op_handlers::mul::mul;
 use op_handlers::near_call::near_call;
-use op_handlers::ok::ok;
 use op_handlers::or::or;
-use op_handlers::panic::{inexplicit_panic, panic};
 use op_handlers::precompile_call::precompile_call;
 use op_handlers::ptr_add::ptr_add;
 use op_handlers::ptr_pack::ptr_pack;
 use op_handlers::ptr_shrink::ptr_shrink;
 use op_handlers::ptr_sub::ptr_sub;
-use op_handlers::revert::revert;
+use op_handlers::ret::ret;
 use op_handlers::shift::rol;
 use op_handlers::shift::ror;
 use op_handlers::shift::shl;
@@ -120,7 +118,7 @@ pub fn run(
         }
 
         if let Some(_err) = vm.decrease_gas(opcode.gas_cost).err() {
-            match inexplicit_panic(&mut vm, storage) {
+            match ret(&mut vm, &opcode, storage, RetOpcode::Panic, false) {
                 Ok(false) => continue,
                 _ => return Ok((ExecutionOutput::Panic, vm)),
             }
@@ -182,34 +180,39 @@ pub fn run(
                     far_call(&mut vm, &opcode, &far_call_variant, storage)
                 }
                 Variant::Ret(ret_variant) => match ret_variant {
-                    RetOpcode::Ok => match ok(&mut vm, &opcode) {
-                        Ok(should_break) => {
-                            if should_break {
-                                break;
-                            }
-                            Ok(())
-                        }
-                        Err(e) => Err(e),
-                    },
-                    RetOpcode::Revert => match revert(&mut vm, &opcode, storage) {
+                    RetOpcode::Ok => match ret(&mut vm, &opcode, storage, RetOpcode::Ok, true) {
                         Ok(should_break) => {
                             if should_break {
                                 let result = retrieve_result(&mut vm)?;
-                                return Ok((ExecutionOutput::Revert(result), vm));
+                                return Ok((ExecutionOutput::Ok(result), vm));
                             }
                             Ok(())
                         }
                         Err(e) => Err(e),
                     },
-                    RetOpcode::Panic => match panic(&mut vm, &opcode, storage) {
-                        Ok(should_break) => {
-                            if should_break {
-                                return Ok((ExecutionOutput::Panic, vm));
+                    RetOpcode::Revert => {
+                        match ret(&mut vm, &opcode, storage, RetOpcode::Revert, true) {
+                            Ok(should_break) => {
+                                if should_break {
+                                    let result = retrieve_result(&mut vm)?;
+                                    return Ok((ExecutionOutput::Revert(result), vm));
+                                }
+                                Ok(())
                             }
-                            Ok(())
+                            Err(e) => Err(e),
                         }
-                        Err(e) => Err(e),
-                    },
+                    }
+                    RetOpcode::Panic => {
+                        match ret(&mut vm, &opcode, storage, RetOpcode::Panic, true) {
+                            Ok(should_break) => {
+                                if should_break {
+                                    return Ok((ExecutionOutput::Panic, vm));
+                                }
+                                Ok(())
+                            }
+                            Err(e) => Err(e),
+                        }
+                    }
                 },
                 Variant::UMA(uma_variant) => match uma_variant {
                     UMAOpcode::HeapRead => heap_read(&mut vm, &opcode),
@@ -222,7 +225,7 @@ pub fn run(
                 },
             };
             if let Err(_err) = result {
-                match inexplicit_panic(&mut vm, storage) {
+                match ret(&mut vm, &opcode, storage, RetOpcode::Panic, false) {
                     Ok(false) => continue,
                     _ => return Ok((ExecutionOutput::Panic, vm)),
                 }
@@ -232,8 +235,6 @@ pub fn run(
             vm.current_frame_mut()?.pc += 1;
         }
     }
-    let result = retrieve_result(&mut vm)?;
-    Ok((ExecutionOutput::Ok(result), vm))
 }
 
 // Sets the next PC according to the next opcode
@@ -242,8 +243,7 @@ fn set_pc(vm: &mut VMState, opcode: &Opcode) -> Result<(), EraVmError> {
 
     vm.current_frame_mut()?.pc = match opcode.variant {
         Variant::FarCall(_) => 0,
-        Variant::Ret(RetOpcode::Revert) => current_pc,
-        Variant::Ret(RetOpcode::Panic) => current_pc,
+        Variant::Ret(_) => current_pc,
         Variant::NearCall(_) => current_pc,
         Variant::Jump(_) => current_pc,
         _ => current_pc + 1,
