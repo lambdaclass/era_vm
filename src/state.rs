b890f9ad436f6377f58b9ed43190fb8e8a0b1cad
Mario Rugiero <mrugiero@gmail.com>
2024-08-13 22:55:24 -0300

    fix: code-page reads must always succeed (#190)
    * fix: code-page reads must always succeed
    
    From the spec:
    > Code pages
    > A code page contains 2^16 instructions. They are not writable.
    > On genesis, code pages are filled as follows:
    > The contract code is places starting from the address 0.
    > The rest is filled with a value guaranteed to decode as invalid
    > instruction.
    
    Our code before this fix panics when running the benchmarks due to this
    discrepancy.
    
    The applied fix creates a newtype for code pages that wraps the
    `Vec<U256>` and returns `U256::zero()` when reading out of bounds, which
    decodes to an invalid instruction, matching the spec and fixing the
    `panic`.
    
    Because test encoding doesn't respect the 2^16 spec, only the out-of-bounds
    behavior is changed.

--- src/state.rs
+++ src/state.rs
@@ -269,24 +269,20 @@ impl VMState {
         // - pc mod 2 ≣ 1 -> Take the low 128 bits of the word and decode the opcode.
         // - pc mod 2 ≣ 0 -> Take the high 128 bits of the word and decode the opcode .
         // U256 provides the low_u128 method which is self-describing.
-        current_context
+        let raw_op = current_context
             .code_page
             // pc / 2
-            .get((pc >> 1) as usize)
-            .ok_or(EraVmError::NonValidProgramCounter)
-            .map(|raw_op| match pc % 2 {
-                1 => raw_op.low_u128(),
-                _ => (raw_op >> 128).low_u128(),
-            })
-            .and_then(Opcode::try_from_raw_opcode_test_encode)
+            .get(pc as usize >> 1);
+        let opcode = match pc % 2 {
+            1 => raw_op.low_u128(),
+            _ => (raw_op >> 128).low_u128(),
+        };
+        Opcode::try_from_raw_opcode_test_encode(opcode)
     }
     pub fn get_opcode(&self) -> Result<Opcode, EraVmError> {
         let current_context = self.current_context()?;
         let pc = self.current_frame()?.pc;
-        let raw_opcode = *current_context
-            .code_page
-            .get((pc / 4) as usize)
-            .ok_or(EraVmError::NonValidProgramCounter)?;
+        let raw_opcode = current_context.code_page.get(pc as usize / 4);
 
         let raw_op = match pc % 4 {
             3 => (raw_opcode & u64::MAX.into()).as_u64(),
