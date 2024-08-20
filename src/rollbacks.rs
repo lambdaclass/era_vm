679d38d0c6175a099ec3d522eb50e66786beb49a
Marcos Nicolau <marcosnicolau@lambdaclass.com>
2024-08-19 22:55:04 -0300

    Reimplement vec snapshots

--- src/rollbacks.rs
+++ src/rollbacks.rs
@@ -14,11 +14,26 @@ pub struct RollbackableHashMap<K: Clone + Hash, V: Clone> {
     pub map: HashMap<K, V>,
 }
 
-impl<K: Clone + Hash, V: Clone> RollbackableHashMap<K, V> {
+impl<K: Clone + Hash + Eq, V: Clone> RollbackableHashMap<K, V> {
     pub fn new() -> Self {
         Self {
             map: HashMap::new(),
         }
+    }
+
+    pub fn get_logs_after_snapshot(
+        &self,
+        snapshot: <RollbackableHashMap<K, V> as Rollbackable>::Snapshot,
+    ) -> HashMap<K, V> {
+        let mut changes = HashMap::new();
+
+        for (key, value) in self.map.iter() {
+            if !snapshot.contains_key(key) || snapshot.get(key).is_none() {
+                changes.insert(key.clone(), value.clone());
+            }
+        }
+
+        changes
     }
 }
 
@@ -51,16 +66,24 @@ impl<T: Clone> RollbackableVec<T> {
             entries: Vec::new(),
         }
     }
+
+    pub fn get_logs_after_snapshot(
+        &self,
+        snapshot: <RollbackableVec<T> as Rollbackable>::Snapshot,
+    ) -> &[T] {
+        &self.entries[snapshot..]
+    }
 }
 
 impl<T: Clone> Rollbackable for RollbackableVec<T> {
-    type Snapshot = Vec<T>;
+    type Snapshot = usize;
 
     fn rollback(&mut self, snapshot: Self::Snapshot) {
-        self.entries = snapshot;
+        self.entries.truncate(snapshot);
     }
+
     fn snapshot(&self) -> Self::Snapshot {
-        self.entries.clone()
+        self.entries.len()
     }
 }
 
