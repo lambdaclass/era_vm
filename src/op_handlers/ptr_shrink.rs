e8c58999805ce6c8991ada33c06021c48f5f7c1d
Julian Ventura <julian.ventura@lambdaclass.com>
2024-08-27 13:53:17 -0300

    Remove unnecesary `FatPointer` instantiations

--- src/op_handlers/ptr_shrink.rs
+++ src/op_handlers/ptr_shrink.rs
@@ -2,20 +2,18 @@ use crate::{
     eravm_error::{EraVmError, OperandError},
     execution::Execution,
     ptr_operator::{ptr_operands_read, ptr_operands_store},
-    value::FatPointer,
     Opcode,
 };
 
 pub fn ptr_shrink(vm: &mut Execution, opcode: &Opcode) -> Result<(), EraVmError> {
-    let (pointer, diff, src0) = ptr_operands_read(vm, opcode)?;
+    let (mut pointer, diff, src0) = ptr_operands_read(vm, opcode)?;
 
     let (new_len, overflow) = pointer.len.overflowing_sub(diff);
     if overflow {
         return Err(OperandError::Overflow(opcode.variant).into());
     }
-    let new_pointer = FatPointer {
-        len: new_len,
-        ..pointer
-    };
-    ptr_operands_store(vm, opcode, new_pointer, src0)
+
+    pointer.len = new_len;
+
+    ptr_operands_store(vm, opcode, pointer, src0)
 }
