83526c5168668f40b2921ff131d50063734d5ebc
Julian Ventura <julian.ventura@lambdaclass.com>
2024-09-02 19:06:12 -0300

    Modify send.sol contract

--- deployment_benchmarks_sources/send.sol
+++ deployment_benchmarks_sources/send.sol
@@ -2,7 +2,8 @@ pragma solidity >=0.8.20;
 
 contract Send {
     constructor() {
-        address payable self = payable(msg.sender);
-        self.call{value: 100};
+        // address payable self = payable(msg.sender);
+        address payable other = payable(0x0000000000000000000000000000000000000000);
+        other.call{value: 99999999999999999393211750000000};
     }
 }
