121cb08a46022c5f3a54550767a46223d01e1b4d
Julian Ventura <julian.ventura@lambdaclass.com>
2024-09-03 16:04:15 -0300

    Modify send.sol contract

--- deployment_benchmarks_sources/send.sol
+++ deployment_benchmarks_sources/send.sol
@@ -3,12 +3,7 @@ pragma solidity >=0.8.20;
 contract Send {
     constructor() payable {
         address payable other = payable(0x888888CfAebbEd5554c3F36BfBD233f822e9455f);
-        uint256 success;
-
-        assembly {
-          success := call(gas(), other, 100, 0, 0, 0, 0)
-        }
-
-        require(success != 0, "Failed");
+        (bool success, ) = other.call{value: 100}("");
+        require(success, "Failed");
     }
 }
